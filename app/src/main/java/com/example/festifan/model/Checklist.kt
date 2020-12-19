ef0d78e0676fa910603aca3de0f7e065ad4a8d40
4libutt <ali@vergelijkgroep.nl>
2020-12-19 14:41:30 +0100

    added checklist data class

---
+++ app/src/main/java/com/example/festifan/model/Checklist.kt
@@ -0,0 +1,33 @@
+package com.example.festifan.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "checklist")
+@Parcelize
+data class Checklist(
+
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        var id: Long? = null,
+
+        @ColumnInfo(name = "name")
+        var name: String,
+
+        @ColumnInfo(name = "created")
+        var created: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "important")
+        var important: Boolean = false,
+
+        @ColumnInfo(name = "completed")
+        var completed: Boolean = false,
+
+) : Parcelable {
+        val createdDateFormatted: String
+        get() = DateFormat.getDateInstance().format(created)
+}
