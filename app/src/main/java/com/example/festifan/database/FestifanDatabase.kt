a7c2f244129d9ede118e9c85bd002fc2dc00f9fc
4libutt <ali@vergelijkgroep.nl>
2020-12-24 13:43:58 +0100

    created FestifanDatabase class

---
+++ app/src/main/java/com/example/festifan/database/FestifanDatabase.kt
@@ -0,0 +1,43 @@
+package com.example.festifan.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.festifan.dao.TaskDao
+import com.example.festifan.model.Task
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Database(entities = arrayOf(Task::class), version = 1, exportSchema = false)
+abstract class FestifanDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        private const val DATABASE_NAME = "FESTIFAN_DATABASE"
+
+
+        @Volatile
+        private var festifanDatabase: FestifanDatabase? = null
+
+        fun getDatabase(context: Context): FestifanDatabase? {
+            if(festifanDatabase == null) {
+                synchronized(FestifanDatabase::class.java) {
+                    if(festifanDatabase == null) {
+                        festifanDatabase = Room.databaseBuilder(
+                            context.applicationContext,
+                            FestifanDatabase::class.java,
+                            DATABASE_NAME
+                        )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    }
+                }
+            }
+            return festifanDatabase
+        }
+    }
+}
