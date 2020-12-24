aced66cfc31826b062b754de5f6329489a9f0895
4libutt <ali@vergelijkgroep.nl>
2020-12-24 13:43:14 +0100

    updated task model

--- app/src/main/java/com/example/festifan/model/Task.kt
+++ app/src/main/java/com/example/festifan/model/Task.kt
@@ -1,31 +1,21 @@
 package com.example.festifan.model
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.text.DateFormat
 
-@Entity(tableName = "checklist")
+@Entity(tableName = "task")
 @Parcelize
 data class Task(
 
-        @PrimaryKey(autoGenerate = true)
-        @ColumnInfo(name = "id")
-        var id: Long? = null,
-
-        @ColumnInfo(name = "name")
         var name: String,
-
-        @ColumnInfo(name = "created")
-        var created: Long = System.currentTimeMillis(),
-
-        @ColumnInfo(name = "important")
         var important: Boolean = false,
-
-        @ColumnInfo(name = "completed")
         var completed: Boolean = false,
+        var created: Long = System.currentTimeMillis(),
+        @PrimaryKey(autoGenerate = true)
+        var id: Long? = null
 
 ) : Parcelable {
         val createdDateFormatted: String
