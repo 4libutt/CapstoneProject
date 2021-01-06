bd9480862a3da42542decbf05914378383466ed9
4libutt <ali@vergelijkgroep.nl>
2021-01-06 16:25:01 +0100

    removed unnecessary imports

--- app/src/main/java/com/example/festifan/database/FestifanDatabase.kt
+++ app/src/main/java/com/example/festifan/database/FestifanDatabase.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.festifan.dao.TaskDao
 import com.example.festifan.model.Task
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Task::class), version = 1, exportSchema = false)
 abstract class FestifanDatabase : RoomDatabase() {
