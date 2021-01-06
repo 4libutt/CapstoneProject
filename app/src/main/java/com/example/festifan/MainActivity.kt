7f30f8c97997245dea6c358fc80c31a88a84798f
4libutt <ali@vergelijkgroep.nl>
2021-01-06 16:19:00 +0100

    added get location method for current lat long

--- app/src/main/java/com/example/festifan/MainActivity.kt
+++ app/src/main/java/com/example/festifan/MainActivity.kt
@@ -1,21 +1,79 @@
 package com.example.festifan
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var locationManager: LocationManager
+    private lateinit var currentLocation: String
+    private var hasGps = false
+    private var hasNetwork = false
+    private var locationGps: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         navController = findNavController(R.id.nav_host_fragment)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                , 111)
+        }
+        getLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        if (hasGps || hasNetwork) {
+
+            if (hasGps) {
+                Log.d("codeAndroidLocation", "hasGps")
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0f
+                ) { location ->
+                    locationGps = location
+                    currentLocation =
+                        locationGps!!.latitude.toString() + "," + locationGps!!.longitude.toString()
+                }
+
+                val localGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (localGpsLocation != null) {
+                    locationGps = localGpsLocation
+                }
+            }
+
+            if (locationGps != null) {
+                currentLocation = locationGps!!.latitude.toString() + "," + locationGps!!.longitude.toString()
+            }
+        } else {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+    }
+
+    fun getWeatherLocation(): String {
+        return currentLocation
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -52,11 +110,9 @@ class MainActivity : AppCompatActivity() {
     private fun toggleNavIcon() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id in arrayOf(R.id.HomeFragment)) {
-                supportActionBar?.hide()
                 supportActionBar?.setDisplayHomeAsUpEnabled(false)
             }
             else {
-                supportActionBar?.show()
                 supportActionBar?.setDisplayHomeAsUpEnabled(true)
             }
         }
